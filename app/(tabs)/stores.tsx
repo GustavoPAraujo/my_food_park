9b37cfc364c5a2517c7f91851c87f8efafb0f410
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-25 20:00:39 -0300

    map marker and size

--- app/(tabs)/stores.tsx
+++ app/(tabs)/stores.tsx
@@ -1,21 +1,34 @@
 import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
-import Map from "react-native-maps"
+import MapView, { Marker, Callout } from "react-native-maps"
+
+
+const coordinate = {
+  latitude: -27.59948942350316,
+  longitude: -48.51859914120166,
+}
 
 const Stores = () => {
   return (
-    <View className='flex-1'>
-
-      <Map 
-        style={StyleSheet.absoluteFill}
+    <View className='w-full h-2/5 '>
+      <MapView
+        style={[StyleSheet.absoluteFill]}
+        className='h-50% w-50%'
         initialRegion={{
-          latitude: -27.5994,
-          longitude: -48.51863,
+          latitude: coordinate.latitude,
+          longitude: coordinate.longitude,
           latitudeDelta: 0.005,
           longitudeDelta: 0.005
-        }}  
-      />
-      
+        }}
+      >
+        <Marker
+          coordinate={coordinate}
+        >
+          <Callout style={styles.callout} >
+          </Callout>
+        </Marker>
+      </MapView >
+
     </View>
   )
 }
@@ -23,6 +36,16 @@ const Stores = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  callout: {
+    width: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  address: {
+    fontSize: 14,
   }
 })
 
